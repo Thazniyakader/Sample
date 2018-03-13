8c50ed9b4f669ba1b00f221df9642b7d2e35c020
tinatinu <36755045+tinatinu@users.noreply.github.com>
2018-03-13 13:19:46 +0530

    new file

---
+++ equal.sh
@@ -0,0 +1,11 @@
+echo "LARGEST NUMBER"
+echo "Enter the first number"
+read a
+echo "Enter the second value"
+read b
+if [ $a == $b ]
+then
+    echo " a is equal to b"
+else
+    echo "not equal"
+fi
